3d13ab6062569b9617d96ed70ee6f31d2c1ad719
AutomatizacionCapacitacion <asanchez@bside.com.mx>
2025-08-17 16:05:04 -0600

    Update scrape.js

--- scrape.js
+++ scrape.js
@@ -3,14 +3,14 @@ const { chromium } = require('playwright');
 (async () => {
   const browser = await chromium.launch();
   const page = await browser.newPage();
-  await page.goto('https://learn.microsoft.com/es-mx/users/abrahamdavidsnchezmoncada-2445/', {
+  await page.goto('https://learn.microsoft.com/es-mx/users/abrahamdavidsnchezmoncada-2445/transcript', {
     waitUntil: 'networkidle',
   });
 
   await page.waitForTimeout(5000); // esperamos que cargue el contenido dinámico
 
   const certs = await page.evaluate(() => {
-    const nodes = [...document.querySelectorAll('.achievement-card .title')];
+    const nodes = [...document.querySelectorAll('#certificaciones-activas-table-record-list td:first-child')];
     return nodes.map(n => n.innerText.trim());
   });
 
